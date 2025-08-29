b0f0dbd2388585364e36226d73d42ccf617f093e
devansh deshmukh <devanshd536@gmail.com>
2025-08-29 11:16:06 +0530

    setinterval

--- script.js
+++ script.js
@@ -1,47 +1,10 @@
-// function findMinMaxLoop(arr) {
-//   if (arr.length === 0) return null;
+let count = 0;
+let intervalId = setInterval(() => {
+  console.log("Repeating message", count);
+  count++;
 
-//   let min = arr[0];
-//   let max = arr[0];
-
-//   for (let i = 1; i < arr.length; i++) {
-//     if (arr[i] < min) min = arr[i];
-//     if (arr[i] > max) max = arr[i];
-//   }
-
-//   return { min, max };
-// }
-
-// // Example usage:
-// const numbers = [10, 25, 3, 99, 56, 1];
-// console.log(findMinMaxLoop(numbers));
-// // Output: { min: 1, max: 99 }
-
-
-
-
-
-// let name = "Jack";
-// let sum = function(a, b){
-//   return a+b;
-// }
-
-// let hello = function(){
-//   console.log("hello");
-// }
-
-
-
-
-
-const student = {
-  name: "menos",
-  age: 21,
-  phy: 89,
-  eng: 95,
-  maths: 90,
-  getAvg(){
-    let avg = (this.phy + this.eng + this.maths)/3;
-    console.log(avg);
+  if (count === 5) {
+    clearInterval(intervalId); // stop after 5 times
+    console.log("Stopped interval");
   }
-}
+}, 1000);
