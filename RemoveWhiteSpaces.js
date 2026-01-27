c7b156c1cbe24fe599ba1ad4b79be7cf4dcf319a
devansh deshmukh <devanshd536@gmail.com>
2026-01-27 12:11:09 +0530

    try

---
+++ RemoveWhiteSpaces.js
@@ -0,0 +1,10 @@
+function removeSpaces(str) {
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== " ") {
+            result += str[i];
+        }
+    }
+    return result;
+}
